6b51597e245a05993646960ae2e22711d1c0f743
Tomáš Kazatel <kazatel@jiktogroup.cz>
2023-07-01 22:34:40 +0200

    feat: initial commit

---
+++ Baserow.ts
@@ -0,0 +1,156 @@
+export type BaserowRecordType<Item> = Item & { id: number };
+
+export interface BaserowListResponse<Item> {
+  count: number;
+  next: null | string;
+  previous: null | string;
+  results: BaserowRecordType<Item>[];
+}
+
+export interface BaserowListParams<Item> {
+  page?: number;
+  size?: number;
+  search?: string;
+  orderBy?: keyof BaserowRecordType<Item>;
+  orderDir?: 'ASC' | 'DESC';
+}
+
+interface BaserowCreateParams {
+  page?: number;
+  size?: number;
+  search?: string;
+  orderBy?: string | number | symbol;
+  orderDir?: 'ASC' | 'DESC';
+}
+
+export class BaserowRecord<Item> {
+  private readonly record: BaserowRecordType<Item>;
+
+  constructor(record: BaserowRecordType<Item>) {
+    this.record = record;
+  }
+
+  public getID(): number {
+    return this.record['id'];
+  }
+
+  public getStringValue(key: keyof Item): string {
+    return this.record[key] as string;
+  }
+
+  public getNumberValue(key: keyof Item): number {
+    return parseFloat(this.record[key] as string);
+  }
+}
+
+export class BaserowTable<Item> {
+  private baserow: Baserow;
+
+  private readonly tableID: number;
+
+  private apiUrl = 'https://api.baserow.io/api/database/rows/table/';
+  constructor(baserow: Baserow, { tableID }: { tableID: number }) {
+    this.baserow = baserow;
+    this.tableID = tableID;
+  }
+
+  private getQueryParams({
+    page = 1,
+    size = 100,
+    search = '',
+    orderBy = 'id',
+    orderDir = 'ASC',
+  }: BaserowCreateParams): string {
+    const params = new URLSearchParams();
+    params.append('page', page.toString());
+    params.append('size', size.toString());
+    params.append('user_field_names', this.baserow.showUserFieldNames.toString());
+    params.append('search', search);
+    if (typeof orderBy !== 'symbol') {
+      params.append('order_by', `${orderDir === 'ASC' ? '+' : '-'}${orderBy}`);
+    }
+    return params.toString();
+  }
+
+  public async list(params: BaserowListParams<Item> = {}): Promise<BaserowRecord<Item>[]> {
+    const response: Response = await fetch(
+      `${this.apiUrl}${this.tableID}/?${this.getQueryParams(params)}`,
+      { method: 'GET', headers: { Authorization: `Token ${this.baserow.apiKey}` } }
+    );
+    const data = await response.json() as BaserowListResponse<Item>;
+    return data.results.map((item) => new BaserowRecord<Item>(item));
+  }
+
+  public async get(id: number): Promise<BaserowRecord<Item>> {
+    const response: Response = await fetch(
+      `${this.apiUrl}${this.tableID}/${id}/?user_field_names=${this.baserow.showUserFieldNames}`,
+      { method: 'GET', headers: { Authorization: `Token ${this.baserow.apiKey}` } }
+    );
+    const data = await response.json() as BaserowRecordType<Item>;
+    return new BaserowRecord<Item>(data);
+  }
+
+  public async create(fields: Record<keyof Item, string | number | Array<number | string> | undefined | boolean>): Promise<BaserowRecord<Item>>{
+    const response: Response = await fetch(
+      `${this.apiUrl}${this.tableID}/?user_field_names=${this.baserow.showUserFieldNames}`,
+      { method: 'POST', headers: { Authorization: `Token ${this.baserow.apiKey}`, 'Content-Type': 'application/json' }, body: JSON.stringify(fields) }
+    );
+    const data = await response.json() as BaserowRecordType<Item>;
+    return new BaserowRecord<Item>(data);
+  }
+
+  public async update(id: number, fields: Record<keyof Item, string | number | Array<number | string> | undefined | boolean>): Promise<BaserowRecord<Item>>{
+    const response: Response = await fetch(
+      `${this.apiUrl}${this.tableID}/${id}/?user_field_names=${this.baserow.showUserFieldNames}`,
+      { method: 'PATCH', headers: { Authorization: `Token ${this.baserow.apiKey}`, 'Content-Type': 'application/json' }, body: JSON.stringify(fields) }
+    );
+    const data = await response.json() as BaserowRecordType<Item>;
+    return new BaserowRecord<Item>(data);
+  }
+
+  public async move(id: number, beforeId: number): Promise<BaserowRecord<Item>>{
+    const response: Response = await fetch(
+      `${this.apiUrl}${this.tableID}/${id}/move/?user_field_names=${this.baserow.showUserFieldNames}&before_id=${beforeId}`,
+      { method: 'PATCH', headers: { Authorization: `Token ${this.baserow.apiKey}` } }
+    );
+    const data = await response.json() as BaserowRecordType<Item>;
+    return new BaserowRecord<Item>(data);
+  }
+
+  public async delete(id: number): Promise<BaserowRecord<Item>>{
+    const response: Response = await fetch(
+      `${this.apiUrl}${this.tableID}/${id}`,
+      { method: 'DELETE', headers: { Authorization: `Token ${this.baserow.apiKey}` } }
+    );
+    const data = await response.json() as BaserowRecordType<Item>;
+    return new BaserowRecord<Item>(data);
+  }
+}
+
+class Baserow {
+  private readonly API_KEY: string;
+
+  private readonly SHOW_USER_FIELD_NAMES: boolean;
+
+  constructor({ apiKey, showUserFieldNames = true }: { apiKey?: string; showUserFieldNames?: boolean; }) {
+    if (!apiKey) {
+      throw new Error('Cant create Baserow instance without apiKey');
+    }
+    this.API_KEY = apiKey;
+    this.SHOW_USER_FIELD_NAMES = showUserFieldNames;
+  }
+
+  get apiKey(): string {
+    return this.API_KEY;
+  }
+
+  get showUserFieldNames(): boolean {
+    return this.SHOW_USER_FIELD_NAMES;
+  }
+
+  public table<Item>(tableID: number): BaserowTable<Item> {
+    return new BaserowTable<Item>(this, { tableID });
+  }
+}
+
+export default Baserow;
--- README.md
+++ README.md
@@ -1,2 +1,167 @@
-# baserow-client
-This is basic Node.js Baserow client
+# Baserow Client
+
+The Baserow Client is a TypeScript library that provides convenient methods for interacting with the Baserow application. It allows you to perform various operations such as listing records, retrieving records, creating new records, editing existing records, moving records, and deleting records.
+
+## Installation
+
+Install the Baserow SDK Client using npm:
+
+```bash
+npm install baserow-client
+```
+
+## Usage
+
+Import the Baserow SDK Client in your TypeScript file:
+
+```typescript
+import Baserow from 'baserow-client';
+```
+
+### Creating a Baserow instance
+
+To use the Baserow SDK Client, you need to create a `Baserow` instance by providing your API key. Optionally, you can specify whether to show user field names. Here's an example:
+
+```typescript
+const baserow = new Baserow({ apiKey: 'YOUR_API_KEY', showUserFieldNames: true });
+```
+
+### Accessing a Baserow table
+
+To interact with a specific table in the Baserow application, use the `table` method on the `Baserow` instance. Pass the table ID as an argument. This will return a `BaserowTable` instance that provides various methods for performing operations on the table. If you provide generic type to `table`, you get strong typings on methods behind. Here's an example:
+
+```typescript
+const table = baserow.table<Item>(tableID);
+```
+
+### Listing records
+
+To retrieve a list of records from the Baserow table, use the `list` method on the `BaserowTable` instance. You can specify various parameters such as page number, page size, search query, order by field, and order direction. The method returns a promise that resolves to an array of `BaserowRecord` instances. Here's an example:
+
+```typescript
+interface Item {
+    Name: string;
+    Age: number;
+    Email: string;
+}
+
+const params: BaserowListParams<Item> = {
+    page: 1,
+    size: 20,
+    search: 'example',
+    orderBy: 'Name',
+    orderDir: 'ASC',
+};
+
+table.list(params)
+    .then((records) => {
+        // Process the list of records
+    })
+    .catch((error) => {
+        // Handle the error
+    });
+```
+
+### Retrieving a record
+
+To retrieve a single record from the Baserow table by its ID, use the `get` method on the `BaserowTable` instance. Pass the record ID as an argument. The method returns a promise that resolves to a `BaserowRecord` instance. Here's an example:
+
+```typescript
+table.get(recordID)
+  .then((record) => {
+    // Process the record
+  })
+  .catch((error) => {
+    // Handle the error
+  });
+```
+
+### Creating a record
+
+To create a new record in the Baserow table, use the `create` method on the `BaserowTable` instance. Pass a fields object that contains the values for each field in the record. The method returns a promise that resolves to a `BaserowRecord` instance representing the newly created record. Here's an example:
+
+```typescript
+interface Item {
+    Name: string;
+    Age: number;
+    Email: string;
+}
+
+const fields: Item = {
+  Name: 'John Doe',
+  Age: 30,
+  Email: 'john@example.com',
+};
+
+table.create(fields)
+  .then((record) => {
+    // Process the created record
+  })
+  .catch((error) => {
+    // Handle the error
+  });
+```
+
+### Editing a record
+
+To edit an existing record in the Baserow table, use the `update` method on the `BaserowTable` instance. Pass the record ID and a
+
+ fields object that contains the updated values for the record. The method returns a promise that resolves to a `BaserowRecord` instance representing the updated record. Here's an example:
+
+```typescript
+interface Item {
+    Name: string;
+    Age: number;
+    Email: string;
+}
+
+const fields: Item = {
+  Name: 'Updated Name',
+  Age: 40,
+  Email: 'updated@example.com',
+};
+
+table.update(recordID, fields)
+  .then((record) => {
+    // Process the updated record
+  })
+  .catch((error) => {
+    // Handle the error
+  });
+```
+
+### Moving a record
+
+To move a record within the Baserow table, use the `move` method on the `BaserowTable` instance. Pass the record ID and the ID of the record before which you want to move the record. The method returns a promise that resolves to a `BaserowRecord` instance representing the moved record. Here's an example:
+
+```typescript
+table.move(recordID, beforeRecordID)
+  .then((record) => {
+    // Process the moved record
+  })
+  .catch((error) => {
+    // Handle the error
+  });
+```
+
+### Deleting a record
+
+To delete a record from the Baserow table, use the `delete` method on the `BaserowTable` instance. Pass the record ID as an argument. The method returns a promise that resolves to a `BaserowRecord` instance representing the deleted record. Here's an example:
+
+```typescript
+table.delete(recordID)
+  .then((record) => {
+    // Process the deleted record
+  })
+  .catch((error) => {
+    // Handle the error
+  });
+```
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+## Contributing
+
+Contributions are welcome!
